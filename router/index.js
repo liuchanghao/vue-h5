e6cbc71401db354afd57d9fac00c1c36329813bd
liuchanghao <2695608803@qq.com>
2020-09-29 14:08:42 +0800

    修改未知错误

--- router/index.js
+++ router/index.js
@@ -1,7 +1,7 @@
-const router = require('koa-router')();   //引入路由函数
+const router = require('koa-router')(); //引入路由函数
 
-const wechat = require('../action/wechat');//微信
-const user = require('../action/user');//会员
+const wechat = require('../action/wechat'); //微信
+const user = require('../action/user'); //会员
 
 //业务接口相关
 router.get('/api/getUserByKey', user.getUserByKey);
@@ -10,6 +10,7 @@ router.get('/api/getReportData', user.getReportData);
 //微信相关
 router.get('/wxapi/test', wechat.test);
 router.get('/wxapi/getOauthUrl', wechat.getOauthUrl);
+
 router.get('/wxapi/getWechatMemberInfo', wechat.getWechatMemberInfo);
 router.get('/wxapi/getWechatSignature', wechat.getWechatSignature);
 
